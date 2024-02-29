fbc1d9dd97e8c1143b0d4d91816c9bf6b178559f
Xhoucw <120392315+pussy-missed@users.noreply.github.com>
2024-02-29 23:48:43 +0300

    TextNew

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
@@ -33,8 +33,8 @@ fun TextScreen() {
 @Composable
 @Preview
 fun MyText() {
-    Text(text = stringResource(id = R.string.set_text))
     Text(
         text = stringResource(id = R.string.set_text)
     )
+
 }
