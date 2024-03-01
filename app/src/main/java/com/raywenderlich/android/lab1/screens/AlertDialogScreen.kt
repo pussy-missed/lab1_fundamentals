d017aeaf131a7f6ffe21515ff43ad35245351f6c
Xhoucw <120392315+pussy-missed@users.noreply.github.com>
2024-03-01 15:54:23 +0300

    AlertDialogNewMake

--- app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
@@ -1,6 +1,5 @@
 package com.raywenderlich.android.lab1.screens
 
-import android.app.AlertDialog
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -10,6 +9,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -34,8 +34,8 @@ fun MyAlertDialog() {
                 shouldShowDialog.value = false
                 FundamentalsRouter.navigateTo(Screen.Navigation)
             },
-            title = { Text(text = stringResource(id = "Alert")) },
-            text = { Text(text = stringResource(id = "Это диалоговое окно")) },
+            title = { Text(text = stringResource(id = R.string.title_alertDialog)) },
+            text = { Text(text = stringResource(id = R.string.alertDialog_about)) },
             confirmButton = {
                 Button(
                     colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
@@ -45,8 +45,8 @@ fun MyAlertDialog() {
                     }
                 ) {
                     Text(
-                        text = stringResource(id = "Закрыть"),
-                        color = Color.White
+                        text = stringResource(id = R.string.massage_button),
+                                color = Color.White
                     )
                 }
             }
