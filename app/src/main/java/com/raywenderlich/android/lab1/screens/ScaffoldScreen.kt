bf038e0d5f7a76549b3492396aa19b636c6517b6
Xhoucw <120392315+pussy-missed@users.noreply.github.com>
2024-05-07 12:24:05 +0300

    AppBars all

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -10,6 +10,11 @@ import com.raywenderlich.android.lab1.router.Screen
 import com.raywenderlich.android.lab1.screens.MyColumn
 import com.raywenderlich.android.lab1.screens.MyRow
 import kotlinx.coroutines.CoroutineScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -43,10 +48,31 @@ fun MyScaffold(){
 
 @Composable
 fun MyTopAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope) {
+    val drawerState = scaffoldState.drawerState
 
+    TopAppBar(
+        navigationIcon = {
+            IconButton(
+                content = {
+                    Icon(
+                        Icons.Default.Menu,
+                        tint = Color.White,
+                        contentDescription = stringResource(id = R.string.menu)
+                    )
+                },
+                onClick = {
+                    scope.launch { if (drawerState.isClosed) drawerState.open() else drawerState.close() }
+                }
+            )
+        },
+        title = { Text(text = stringResource(id = R.string.MyComposeApp), color = Color.White )},
+        backgroundColor =  colorResource(id = R.color.colorPrimary)
+    )
 }
 
 @Composable
 fun MyBottomAppBar() {
-
+    BottomAppBar (
+        content = {},
+        backgroundColor = colorResource(id = R.color.colorPrimary))
 }
